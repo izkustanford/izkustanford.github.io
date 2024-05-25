af6f368d19a3c1fad064768c9866dfe959e9b083
Andrew Borisenko <seigiard@gmail.com>
2024-05-25 18:02:05 +0200

    Add swup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,17 @@
-import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import swup from '@swup/astro';
+import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   markdown: {
     remarkPlugins: [setLayout],
   },
   site: 'https://izkustanford.com/',
-  integrations: [sitemap()],
+  integrations: [sitemap(), swup()],
 });
 
 function setLayout() {
   return function (_, file) {
-    console.log(getFileLayout(file));
     file.data.astro.frontmatter.layout = getFileLayout(file);
   };
 }
