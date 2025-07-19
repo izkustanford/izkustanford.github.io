9e462941fb04cd3e6d521a0381472c35f41c763c
Borisenko Andrew <seigiard@gmail.com>
2025-07-19 18:42:57 +0200

    Fix redirect

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,31 @@
-import { defineConfig } from 'astro/config';
-
+import {
+    defineConfig
+} from 'astro/config';
 export default defineConfig({
-  site: 'https://izkustanford.com/',
+    markdown: {
+        remarkPlugins: [setLayout],
+    },
+    site: 'https://izkustanford.com/',
 });
+
+function setLayout() {
+    return function(_, file) {
+        file.data.astro.frontmatter.layout = getFileLayout(file);
+    };
+}
+
+function getFileLayout(file) {
+    const defaultLayout = '@layouts/Default.astro';
+    const initialLayout = file?.data?.astro?.frontmatter?.layout;
+    const profileLayout = '@layouts/Profile.astro';
+    const isProlifeFolder = file?.history?.[0].includes('pages/people');
+    if (initialLayout && initialLayout !== defaultLayout) {
+        return file.data.astro.frontmatter.layout;
+    }
+    if (isProlifeFolder) {
+        return initialLayout && initialLayout !== profileLayout ?
+            initialLayout :
+            profileLayout;
+    }
+    return defaultLayout;
+}
